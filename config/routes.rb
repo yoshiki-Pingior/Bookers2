6cc459d085ec65b23662071a4328e571c8eb98e3
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-04-13 15:13:56 +0000

    [add]aikon

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
   root to: 'homes#top'
+  get'homes/about'
+
   devise_for :users
 
-  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update] 
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
 
   resources :users, only: [:index, :show, :edit, :update]
 
